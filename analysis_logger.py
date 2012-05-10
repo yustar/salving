1f4f248a9733ea8db5b382c5f93db39634b22a6c
yustarwang <yustar.info@gmail.com>
2012-05-10 13:33:07 +0800

    init

---
+++ analysis_logger.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#python log module
+import logging.config
+from logging.handlers import RotatingFileHandler
+import ConfigParser
+import os.path
+
+
+logging.basicConfig(
+		level = logging.INFO,
+		)
+
+#log setting
+def logger():
+	config = ConfigParser.ConfigParser()
+	config.read(os.path.split(os.path.realpath(__file__))[0]+"/analysis.conf")
+	format       = config.get("log", "format").replace('@', '%')
+	name         = config.get("log", "name")
+	backupcount  = int(config.get("log", "backupcount"))
+	maxbytes     = int(config.get("log", "maxbytes"))
+
+	logger = logging.getLogger()
+	Rthandler = RotatingFileHandler(os.path.split(os.path.realpath(__file__))[0]+'/'+name, 
+			maxBytes=maxbytes,
+			backupCount=backupcount)
+	Rthandler.setLevel(logging.INFO)
+	formatter = logging.Formatter(format)
+	Rthandler.setFormatter(formatter)
+	logger.addHandler(Rthandler)
+
+if __name__ == "__main__":
+	print "This program is being run by itself"
+
+version = "1.0.1"
